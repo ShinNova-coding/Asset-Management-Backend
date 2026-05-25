ee8ff0a1c4d6c181833e847f0452ca32d29c0e6d
ShinNova-coding <shweyeeshinthant2@gmail.com>
2026-05-25 16:28:17 +0630

    start my space

--- app/Http/Controllers/CheckWarrantyExpiryController.php
+++ app/Http/Controllers/CheckWarrantyExpiryController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Asset;
 use Illuminate\Http\Request;
 
 class CheckWarrantyExpiryController extends Controller
 {
-    //
-}
+    public function checkWarrantyExpiry($id)
+    {
+        $currentDate = now();
+
+        $assets = Asset::where('asset_id', $id)
+            ->whereRaw('DATE_ADD(purchase_date, INTERVAL warranty_period MONTH) <= ?', [$currentDate])
+            ->where('status', '!=', 'disposed')
+            ->get();
+
+            if($assets){
+                    $assets->update(['status'=>'retired']);
+                }
+                return response()->json([
+                    'success' => true,
+                    'message' => 'The assignment has been retired due to warranty expiry'
+                ]);
+            }
+       
+    }
+
